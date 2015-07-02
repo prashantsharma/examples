920c6882fc916a68fd66867dcbda0869dac43f16
Prashant Sharma <prashant-sha@hcl.com>
2015-07-02 16:46:06 +0530

    File formated

--- javascripting/function-arguments.js
+++ javascripting/function-arguments.js
@@ -1,7 +1,6 @@
-function math (a, b, c) {
-	var mult = b*c;
-
-	return mult+a;
+function math(a, b, c) {
+    var mult = b * c;
+    return mult + a;
 }
 
-console.log(math(53,61,67));
+console.log(math(53, 61, 67));
